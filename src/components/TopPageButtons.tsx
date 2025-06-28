885d2271b48e8876fa0e7e246f8ee39c677a444a
umatoma113 <tomatonikomi113@gmail.com>
2025-06-28 16:25:25 +0900

    TopPageButtonsでのルート処理をonClickからLinkへ変更

--- src/components/TopPageButtons.tsx
+++ src/components/TopPageButtons.tsx
@@ -1,24 +1,16 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function TopPageButtons() {
-  const router = useRouter();
-
   return (
     <div className="flex justify-end gap-4 px-6 py-4">
-      <button
-        onClick={() => router.push('/')}
-        className="text-blue-600 hover:underline"
-      >
+      <Link href="/" className="text-blue-600 hover:underline">
         トップページ
-      </button>
-      <button
-        onClick={() => router.push('/mypage')}
-        className="text-blue-600 hover:underline"
-      >
+      </Link>
+      <Link href="/mypage" className="text-blue-600 hover:underline">
         マイページ
-      </button>
+      </Link>
     </div>
   );
 }
